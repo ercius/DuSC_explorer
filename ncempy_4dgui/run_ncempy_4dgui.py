33d9920084d4be063b7845245186038d581053a2
Peter Ercius razer <percius@lbl.gov>
2022-02-25 11:21:19 -0800

    add script to run the program for debugging

---
+++ ncempy_4dgui/run_ncempy_4dgui.py
@@ -0,0 +1,7 @@
+"""A script to run the GUI. Mainly for debugging. The entrypoint script is the preferred way of
+running program. Just type 4dcamera_gui at the command line."""
+
+from ncempy_4dgui import main
+
+if __name__ == '__main__':
+    main()
