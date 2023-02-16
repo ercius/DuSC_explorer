e571bcc24487529d7c2394ad3842d5d7c4b70dd7
Peter Ercius <peter.ercius@gmail.com>
2023-02-16 14:41:51 -0800

    Update numba and pyqtgraph (#10)
    * add script for minimum gui testing
    
    * Remove the use of an ImageView. ImageItem is all that is necessary for pyqtgraph 0.13 because it allows setColorMap
    
    * fix on_use_colormap function
    
    * update to use pyqtgraph 0.13 and numba 0.56 or newer
    
    * update to use pyqtgraph 0.13 and numba 0.56 or newer
    
    * fix typo
    
    ---------
    
    Co-authored-by: Peter Ercius <percius@lbl.gov>

--- ncempy_4dgui/__init__.py
+++ ncempy_4dgui/__init__.py
@@ -43,7 +43,7 @@ class fourD(QWidget):
 
         self.available_colormaps = ['thermal', 'flame', 'yellowy', 'bipolar', 'spectrum', 'cyclic', 'greyclip', 'grey',
                                     'viridis', 'inferno', 'plasma', 'magma']
-        self.colormap = 'viridis'
+        self.colormap = 'viridis' # default colormap
 
         super(fourD, self).__init__(*args, *kwargs)
         self.setWindowTitle("Stempy: Sparse 4D Data Explorer")
@@ -60,20 +60,22 @@ class fourD(QWidget):
         self.view2 = self.graphics.addViewBox(row=0, col=1, invertY=True)
 
         self.real_space_image_item = pg.ImageItem(border=pg.mkPen('w'))
-        self.real_space_image_view = pg.ImageView(imageItem=self.real_space_image_item)
+        #self.real_space_image_view = pg.ImageView(imageItem=self.real_space_image_item)
         self.view.addItem(self.real_space_image_item)
         self.real_space_image_item.setImage(np.zeros((100, 100), dtype=np.uint32))
         self.view.setAspectLocked()
-        self.real_space_image_view.setPredefinedGradient('viridis')
-
-        self.diffraction_pattern_imageview = pg.ImageItem(border=pg.mkPen('w'))
-        self.diffraction_space_image_view = pg.ImageView(imageItem=self.diffraction_pattern_imageview)
-        self.view2.addItem(self.diffraction_pattern_imageview)
-        self.diffraction_pattern_imageview.setImage(np.zeros((100, 100), dtype=np.uint32))
+        #self.real_space_image_view.setPredefinedGradient('viridis')
+        self.real_space_image_item.setColorMap(self.colormap)
+        
+        self.diffraction_pattern_image_item = pg.ImageItem(border=pg.mkPen('w'))
+        #self.diffraction_space_image_view = pg.ImageView(imageItem=self.diffraction_pattern_imageview)
+        self.view2.addItem(self.diffraction_pattern_image_item)
+        self.diffraction_pattern_image_item.setImage(np.zeros((100, 100), dtype=np.uint32))
         self.view2.setAspectLocked()
-        self.diffraction_space_image_view.setPredefinedGradient('viridis')
-
-        self.diffraction_pattern_imageview.setOpts(axisOrder="row-major")
+        #self.diffraction_space_image_view.setPredefinedGradient('viridis')
+        self.diffraction_pattern_image_item.setColorMap(self.colormap)
+        
+        self.diffraction_pattern_image_item.setOpts(axisOrder="row-major")
         self.real_space_image_item.setOpts(axisOrder="row-major")
 
         self.statusBar = QStatusBar()
@@ -147,8 +149,8 @@ class fourD(QWidget):
 
     def _on_use_colormap(self):
         action = self.sender()
-        self.diffraction_space_image_view.setPredefinedGradient(action.text())
-        self.real_space_image_view.setPredefinedGradient(action.text())
+        self.diffraction_space_image_item.setColorMap(action.text())
+        self.real_space_image_item.setColorMap(action.text())
 
     def _on_export(self):
         """Export the shown diffraction pattern as raw data in TIF file format"""
@@ -337,9 +339,9 @@ class fourD(QWidget):
         self.dp = self.dp.sum(axis=(0, 1))
 
         if self.log_diffraction:
-            self.diffraction_pattern_imageview.setImage(np.log(self.dp + 1), autoRange=True)
+            self.diffraction_pattern_image_item.setImage(np.log(self.dp + 1), autoRange=True)
         else:
-            self.diffraction_pattern_imageview.setImage(self.dp, autoRange=True)
+            self.diffraction_pattern_image_item.setImage(self.dp, autoRange=True)
 
     def update_diffr_jit(self):
         self.dp[:] = self.getDenseFrame_jit(
@@ -349,9 +351,9 @@ class fourD(QWidget):
 
         im = self.dp.reshape(self.frame_dimensions)
         if self.log_diffraction:
-            self.diffraction_pattern_imageview.setImage(np.log(im + 1), autoRange=True)
+            self.diffraction_pattern_image_item.setImage(np.log(im + 1), autoRange=True)
         else:
-            self.diffraction_pattern_imageview.setImage(im, autoRange=True)
+            self.diffraction_pattern_image_item.setImage(im, autoRange=True)
 
     def update_real_stempy(self):
         """ Update the real space image by summing in diffraction space
--- setup.py
+++ setup.py
@@ -73,8 +73,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['stempy>=3.0', 'pyqtgraph>=0.11, <0.12', 'tifffile', 'h5py>=2.9.0',
-                      'numpy', 'qtpy', 'numba<=0.54.1', 'PyQt6'],
+    install_requires=['stempy>=3.0', 'pyqtgraph>=0.13', 'tifffile', 'h5py>=2.9.0',
+                      'numpy', 'qtpy', 'numba>=0.56', 'PyQt5'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
