4a1acbbbd57a16112b6109f4dc99d367a32405e8
Peter Ercius ncem-gauss jupyter <percius.lbl.gov>
2023-12-19 10:57:41 -0800

    fix desc typos

--- run_DuSC_explorer.py
+++ run_DuSC_explorer.py
@@ -1,7 +1,7 @@
 """A script to run the GUI. Mainly for debugging. The entrypoint script is the preferred way of
 running program. """
 
-from desc_explorer import main
+from dusc_explorer import main
 
 if __name__ == '__main__':
     main()
